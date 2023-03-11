4f7a9156f98ecdfd3e38753d7835ef8da591591a
your_username <guilhermev2huehue@gmail.com>
2023-03-11 08:16:22 -0300

    criando servidor local de desenvolvimento

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
     <h1 id="title">
-      <span>ola mundo</span>
+      <span>ola mundooo</span>
     </h1>,
     document.getElementById('root'),
 );
