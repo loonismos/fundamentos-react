116e00421761856311a90a4abfd3040598f216b0
your_username <guilhermev2huehue@gmail.com>
2023-03-13 09:03:49 -0300

    implementando a troca de temas

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Footer() {
+export default function Footer({
+  onToggleTheme, selectedTheme
+ }) {
   return (
     <Container>
       <span>Death Grips' Blog. Todos os direitos reservados.</span>
-      <button type="button">🌞</button>
+      <button 
+        type="button"
+        onClick={onToggleTheme}
+      >
+        {selectedTheme === 'dark' ? '🌞' : '🌚'}
+      </button>
     </Container>
   );
 }
