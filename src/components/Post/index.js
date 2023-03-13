82a87ab9ec3ed45d88b03a0df397568dc4d0a8e3
your_username <guilhermev2huehue@gmail.com>
2023-03-13 07:43:47 -0300

    criando styled components

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -4,14 +4,16 @@ import PropTypes from "prop-types";
 import PostHeader from "./PostHeader";
 import styles from './Post.scss';
 
+import { Subtitle, Rate } from './styles'
 export default function Post(props) {
   return (  
       <article 
-      className={
-        props.post.removed
-          ? styles.postDeleted
-          : styles.post
-      }>
+        className={
+          props.post.removed
+            ? styles.postDeleted
+            : styles.post
+        }
+      >
         <PostHeader 
           onRemove={props.onRemove}
           post={{
@@ -20,10 +22,9 @@ export default function Post(props) {
             read: props.post.read
           }}
         />
-        <br />
-        <small>{props.post.subtitle}</small>
-        <br />
-        Media: {props.post.likes / 2}
+
+        <Subtitle>{props.post.subtitle}</Subtitle>
+        <Rate>Media: {props.post.likes / 2}</Rate>
       </article>
   );
 }
